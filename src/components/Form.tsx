2ff70378e16629976217c7316dd106b46517b4ee
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-05-04 14:12:45 -0400

    fixed

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,32 +1,24 @@
-import React, { useState, FormEvent} from "react"
-
+import React, { FormEvent } from "react"
+import { useState } from "react"
 import { HelloProps } from "../types/FormDataTypes"
 
 export const Form = () => {
-    const [formData, setFormData] = useState<HelloProps>({firstName: '', lastName: '' , age: 0})
+    const [formData, setFormData] = useState<HelloProps>({firstName: '', lastName: '', age: 0})
 
-    const handleFormSubmit = (e: FormEvent<HTMLButtonElement>) => {
+    const handleFormSubmit = (e: FormEvent<HTMLButtonElement>) => { 
         e.preventDefault()
 
-    }    
 
-    return(
+    }
+
+    return (
         <>
-            <form action="submit">
-                <input
-                    type="text"
-                    placeholder="first name"
-                    onChange={(e) => setFormData({...formData, firstName: e.target.value})}/>
-                <input
-                    type="text"
-                    placeholder="last name"
-                    onChange={(e) => setFormData({...formData, lastName: e.target.value})}/>
-                <input
-                    type="number"
-                    placeholder="age - optional"
-                    onChange={(e) => setFormData({...formData, age: e.target.value})}/>
-                <button type='submit' onClick={(e) => handleFormSubmit(e)}>Submit</button>
-            </form>
+        <form action="submit">
+            <input type="text" placeholder="first name" onChange={(e) => setFormData({...formData, firstName: e.target.value})}/>
+            <input type="text" placeholder="last name" onChange={(e) => setFormData({...formData, lastName: e.target.value})}/>
+            <input type="number" placeholder="age - optional" onChange={(e) => setFormData({...formData, age: Number(e.target.value) })}/>
+            <button type="submit" onClick={(e) => handleFormSubmit(e)}>Send Form</button>
+        </form>
         </>
     )
 }
